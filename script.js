ae716f709ed1d928a0f60b813c3f2a94215558d8
wariyoo <wariyoo404@gmail.com>
2025-09-06 13:38:54 +0300

    add Eventlistener to menuIcon

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
-menuIcon.onclick =() {
+menuIcon.addEventListener(onclick, function(){
   navLinks.classList.toggle('active');
-}
+})
+// menuIcon.onclick =() {
+//   navLinks.classList.toggle('active');
+// }
